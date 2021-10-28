31363bc37532c50e3e16b1a1711a56854ef886e9
Katrine Collins <katrine.collins1@gmail.com>
2021-10-28 19:46:22 -0400

    connections

--- Main/controllers/api/comment-routes.js
+++ Main/controllers/api/comment-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Comment } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, (req, res) => {
+router.get("/", (req, res) => {
   Comment.create({ ...req.body, userId: req.session.userId })
     .then(newComment => {
       res.json(newComment);
