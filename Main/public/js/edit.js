84d20033752dee989ef9cd8832c46e2b8a33b52e
Katrine Collins <katrine.collins1@gmail.com>
2021-10-28 23:39:49 -0400

    need seeds

--- Main/public/js/edit.js
+++ Main/public/js/edit.js
@@ -1,36 +1,27 @@
-const postId = document.querySelector('input[name="post-id"]').value;
-
-const editFormHandler = async function(event) {
+async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const body = document.querySelector('textarea[name="post-body"]').value;
-
-  await fetch(`/api/post/${postId}`, {
-    method: 'PUT',
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_url = document.querySelector('textarea[name="post-url"]').value.trim();
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "PUT",
     body: JSON.stringify({
       title,
-      body
+      post_url
     }),
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     }
   });
 
-  document.location.replace('/dashboard');
-};
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
+}
 
-const deleteClickHandler = async function() {
-  await fetch(`/api/post/${postId}`, {
-    method: 'DELETE'
-  });
-
-  document.location.replace('/dashboard');
-};
-
-document
-  .querySelector('#edit-post-form')
-  .addEventListener('submit', editFormHandler);
-document
-  .querySelector('#delete-btn')
-  .addEventListener('click', deleteClickHandler);
+document.querySelector(".edit-post-form").addEventListener("submit", editFormHandler);
